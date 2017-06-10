da2bc7eb36c52cd99d975ee4462f511e14c8e38d
blanklist <joehineriv@gmail.com>
2017-06-10 14:29:20 -0700

    create categories and articles, user auth

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,3 +2,14 @@ get '/categories' do
   @categories = Category.all
   erb :'categories/index'
 end
+
+get '/categories/:category_id/articles' do
+  @category = Category.find_by(params[:id])
+  @articles = @category.articles
+  erb :'articles/index'
+end
+
+# get '/categories/:category_id/articles/:id' do
+
+#   erb :'articles/show'
+# end
