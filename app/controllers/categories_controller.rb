abe1c4d5e1278a6d39515be25f4519e1e8fa74fc
blanklist <joehineriv@gmail.com>
2017-06-10 17:31:49 -0700

    create new articles

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,17 +1,41 @@
+#Index - Category
 get '/categories' do
   @categories = Category.all
   erb :'categories/index'
 end
 
+#Index - Article
 get '/categories/:category_id/articles' do
-  @category = Category.find_by(params[:id])
+  @category = Category.find(params[:category_id])
   @articles = @category.articles
   erb :'articles/index'
 end
 
+#New
+get '/categories/:category_id/articles/new' do
+  @category = Category.find(params[:category_id])
+  erb :'articles/new'
+end
+
+#Create
+post '/categories/:category_id/articles' do
+  p params
+  @category = Category.find(params[:category_id])
+  @article = Article.new(params[:article])
+  if @article.save!
+    redirect "/categories/#{@category.id}/articles"
+  else
+    @errors = @articles.errors.full_messages
+    erb :'articles/new'
+  end
+
+end
+
+#Show
 get '/categories/:category_id/articles/:id' do
   @category = Category.find(params[:category_id])
   @article = Article.find(params[:id])
   @user = @article.user.username
   erb :'articles/show'
 end
+
